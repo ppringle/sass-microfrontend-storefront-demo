504df4a28aa432bf2c7874c20e1b4eae0e47f89b
Pia Pringle <ppringle@vmware.com>
2023-09-03 22:59:13 -0400

    Adding Header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import {StylesProvider, createGenerateClassName} from "@material-ui/core/styles";
+import {BrowserRouter} from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+})
 
 const App = () => {
     return (
-        <div>
-            <h1>Hi There !</h1>
-            <hr/>
-            <MarketingApp/>
-        </div>
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header/>
+                    <MarketingApp/>
+                </div>
+            </StylesProvider>
+        </BrowserRouter>
     )
 }
 
