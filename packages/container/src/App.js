753e44344c63e236d1eee2103881c1734408332f
Pia Pringle <ppringle@vmware.com>
2023-09-03 23:59:14 -0400

    Adding changes to the Material UI configuration for prod which makes each module's css unique between different micro-frontends

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,15 +3,15 @@ import {StylesProvider, createGenerateClassName} from "@material-ui/core/styles"
 import {BrowserRouter} from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
-//
-// const generateClassName = createGenerateClassName({
-//     productionPrefix: 'co',
-// })
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+})
 
 const App = () => {
     return (
         <BrowserRouter>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header/>
                     <MarketingApp/>
