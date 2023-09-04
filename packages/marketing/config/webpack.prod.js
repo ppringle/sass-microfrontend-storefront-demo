1d53b8824fa91e10085629bbdd8d441d799df0b6
Pia Pringle <ppringle@vmware.com>
2023-09-03 23:55:19 -0400

    Setting mode to production in marketing webconfig for prod (webpack)

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -4,7 +4,7 @@ const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common');
 
 const prodConfig = {
-    mode: 'development',
+    mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
         publicPath: '/marketing/latest/',
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -4,13 +4,13 @@ import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles'
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
-const generateClassName = createGenerateClassName({
-    productionPrefix: 'ma',
-})
+// const generateClassName = createGenerateClassName({
+//     productionPrefix: 'ma',
+// })
 
 export default() => {
     return <div>
-        <StylesProvider generateClassName={generateClassName}>
+        <StylesProvider>
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/pricing" component={Pricing}/>
