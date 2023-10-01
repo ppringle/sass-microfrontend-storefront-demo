8335d88441c03f95762d941101590f421d1dc8a7
Pia Pringle <ppringle@vmware.com>
2023-10-01 12:48:07 -0400

    Adds security around access to the dashboard application from the container app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,7 @@
-import React, {lazy, Suspense, useState} from 'react';
+import React, {lazy, Suspense, useState, useEffect} from 'react';
 import {StylesProvider, createGenerateClassName} from "@material-ui/core/styles";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {Router, Route, Switch, Redirect} from "react-router-dom";
+import {createBrowerHistory} from 'history';
 
 import Header from "./components/Header";
 import Progress from "./components/Progress";
@@ -13,12 +14,20 @@ const AuthLazy = lazy(() => import('./components/AuthApp'));
 const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const DashboardLazy = lazy(() => import('./components/DashboardApp'));
 
+const history = createBrowerHistory();
+
 const App = () => {
 
     const [isSignedIn, setIsSignedIn] = useState(false);
 
+    useEffect(() => {
+        if (isSignedIn) {
+            history.push('/dashboard');
+        }
+    }, [isSignedIn])
+
     return (
-        <BrowserRouter>
+        <Router history={history}>
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)}/>
@@ -27,13 +36,16 @@ const App = () => {
                             <Route path="/auth">
                                 <AuthLazy onSignIn={() => setIsSignedIn(true)}/>
                             </Route>
-                            <Route path="/dashboard" component={DashboardLazy}/>
+                            <Route path="/dashboard">
+                                {!isSignedIn && <Redirect to="/"/>}
+                                <DashboardLazy/>
+                            </Route>
                             <Route path="/" component={MarketingLazy}/>
                         </Switch>
                     </Suspense>
                 </div>
             </StylesProvider>
-        </BrowserRouter>
+        </Router>
     )
 }
 
